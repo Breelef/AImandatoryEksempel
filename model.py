33680129f8cd72f3602b6b62369fa7df3c32adc0
Breelef <92298494+Breelef@users.noreply.github.com>
2024-04-05 23:00:26 +0200

    fixes

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+
 
 file_path = 'cars.csv'
 data = pd.read_csv(file_path)
